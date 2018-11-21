b78f58f72e43ddeb11821ee57a203b603df3cba6
shengqing <shengoo@hotmail.com>
2018-11-21 14:21:06 +0800

    add redux saga demo

--- redux-saga-demo/src/index.js
+++ redux-saga-demo/src/index.js
@@ -2,7 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+import reducer from './reducers'
+import rootSaga from './sagas'
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
+// mount it on the Store
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
+
+// then run the saga
+sagaMiddleware.run(rootSaga)
+
+const Root = () => (
+  <Counter
+      value={store.getState()}
+      onIncrement={() => action('INCREMENT')}
+      onDecrement={() => action('DECREMENT')}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')} />
+)
+
+const action = type => store.dispatch({type})
+
+const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) =>(
+  <div>
+    <button onClick={onIncrementAsync}>
+      Increment after 1 second
+    </button>
+    {' '}
+    <button onClick={onIncrement}>
+      Increment
+    </button>
+    {' '}
+    <button onClick={onDecrement}>
+      Decrement
+    </button>
+    <hr />
+    <div>
+      Clicked: {value} times
+    </div>
+  </div>
+);
+
+// render the application
+function render(){
+  ReactDOM.render(<Root />, document.getElementById('root'));
+}
+render()
+store.subscribe(render)
---
+++ redux-saga-demo/src/sagas.js
@@ -0,0 +1,28 @@
+import { delay } from 'redux-saga'
+import { put, takeEvery, all, takeLatest } from 'redux-saga/effects'
+
+
+export function* helloSaga() {
+  console.log('Hello Sagas!')
+}
+
+
+// Our worker Saga: will perform the async increment task
+export function* incrementAsync() {
+  yield delay(1000)
+  yield put({ type: 'INCREMENT' })
+}
+
+// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
+export function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+// notice how we now only export the rootSaga
+// single entry point to start all Sagas at once
+export default function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
+}
